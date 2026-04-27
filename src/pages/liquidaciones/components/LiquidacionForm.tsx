4623ab57143ea128c37d15a06f814849eff1904a
david3010M <hvaldiviezos@unprg.edu.pe>
2026-04-27 15:33:00 -0500

    Update LiquidacionForm.tsx

--- src/pages/liquidaciones/components/LiquidacionForm.tsx
+++ src/pages/liquidaciones/components/LiquidacionForm.tsx
@@ -103,7 +103,6 @@ export default function LiquidacionForm({ onSuccess }: LiquidacionFormProps) {
       onSuccess?.();
     },
     onError: (error: any) => {
-      console.log(error);
       const errorMessage =
         error?.response?.data?.message ||
         error?.message ||

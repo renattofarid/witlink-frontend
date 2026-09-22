a151f16249005d1dd2943fb137b6d86091716866
FrankOcrospomaUgaz <anthony.2005.rap@gmail.com>
2026-09-22 14:12:53 -0500

    feat: add NavUser component with user details, theme selector, and logout functionality

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -55,7 +55,7 @@ export function NavUser({ user }: { user: AuthUsuario }) {
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">{`${user.nombre_usuario}`}</span>
-                <span className="truncate text-xs">{user.nombre_usuario}</span>
+                <span className="truncate text-xs">{user.tipo_usuario?.nombre}</span>
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -81,7 +81,7 @@ export function NavUser({ user }: { user: AuthUsuario }) {
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">{`${user.nombre_usuario}`}</span>
                   <span className="truncate text-xs">
-                    {user.nombre_usuario}
+                    {user.tipo_usuario?.nombre}
                   </span>
                 </div>
               </div>
